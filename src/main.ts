198f1882a6ddcd73490ae28c2ac2025b926c3898
ayman.kaddioui <ayman.kaddioui@it.devcorp.fr>
2022-08-06 09:59:46 +0100

    refactor: add cors & mongoExceptionFilter & ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { MongoExceptionFilter } from './filters/mongo-exception.filter';
+import { TransformInterceptor } from './interceptors/transform.interceptor';
 
+const port = parseInt(process.env.PORT) || 3000;
+const globalPrefix = 'api';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app
+    .setGlobalPrefix(globalPrefix)
+    .useGlobalFilters(new MongoExceptionFilter())
+    .useGlobalInterceptors(new TransformInterceptor())
+    .useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        whitelist: true,
+        forbidNonWhitelisted: true,
+      }),
+    );
+  await app.listen(port);
+  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 bootstrap();
